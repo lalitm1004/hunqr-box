9d10ec4cec8338e336ded553f2347e7b4aa12dd7
Lalit M <lalitm1004@gmail.com>
2024-03-05 22:02:58 +0530

    Add assetlinks

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { useRouter } from "next/navigation"
+import { useEffect } from "react";
+
+export default function Custom404 () {
+  
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/");
+  }, [])
+  
+  return (
+    <div>
+      No
+    </div>
+  )
+}
