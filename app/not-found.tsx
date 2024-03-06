402fe6ed855d4641a86feddc17555e5832716457
Lalit M <lalitm1004@gmail.com>
2024-03-06 10:52:01 +0530

    Simple fixes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,8 +11,12 @@ export default function Custom404 () {
   }, [])
   
   return (
-    <div>
-      No
-    </div>
+    <main>
+      <div className={`h-screen w-screen bg-hungerbox-violet flex justify-center items-center`}>
+        <div className={`h-[60%] w-[80%] md:w-[50%] lg:w-[20%] bg-white rounded-xl shadow-lg shadow-black flex flex-col justify-center items-center`}>
+          <h1 className={`text-3xl`}>404</h1>
+        </div>
+      </div>
+    </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,50 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { QrCode } from "lucide-react";
+import { CloudSnow, QrCode } from "lucide-react";
 import { useQRCode } from "next-qrcode";
 import React, { ChangeEvent, useEffect, useState } from "react";
 
 export default function Home() {
 
-  const [input, setInput] = useState<string | null>(null);
-  const [validInput, setValidInput] = useState<boolean>(false);
-
   const { Canvas } = useQRCode(); 
+  const [input, setInput] = useState<string>("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const inputValue = event.target.value;
-    if (/^\d*$/.test(inputValue)) {
-      setInput(inputValue);
+    const value = event.target.value;
+    const latest = value[value.length - 1];
+    console.log(latest);
+    if (value.length > 4) {
+      event.target.value = value.slice(0, 4);
+    } else if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", undefined].indexOf(latest) === -1) {
+      event.target.value = value.slice(0, value.length - 1);
+    } else if (value.length === 0) {
+      setInput("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    } else {
+      setInput(value);
     }
   }
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (
-      !(
-        (event.key >= '0' && event.key <= '9') ||
-        event.key === "Backspace" ||
-        event.key === "Delete" ||
-        event.key === "ArrowLeft" ||
-        event.key === "ArrowRight" ||
-        event.key === "Tab"
-      )
-    ) {
-      event.preventDefault();
-    }
-  }
-
-  useEffect(() => {
-    setValidInput(input?.length === 4);
-  }, [input])
 
   return (
     <main>
       <div className={`h-screen w-screen bg-hungerbox-violet flex justify-center items-center`}>
         <div className={`h-[60%] w-[80%] md:w-[50%] lg:w-[20%] bg-white rounded-xl shadow-lg shadow-black flex flex-col justify-between`}>
           <div className={`text-3xl w-full flex justify-center items-center gap-2 my-5`}>
-            <QrCode
-              color={`#3A1593`}
-            />
-            <p>HunQRBox</p>
+            <QrCode color={`#3A1593`}/>
+            <h1>HunQRBox</h1>
           </div>
           <div className={`flex flex-grow flex-col justify-start mt-5`}>
-            <div
-              className={`flex justify-center items-center gap-2`}
-            >
+            <div className={`flex justify-center items-center gap-2`}>
               <Input
                 type="text"
-                maxLength={4}
+                inputMode="numeric"
+                pattern="\d{4}"
                 placeholder={`Enter Code`}         
-                className={`bg-hungerbox-light-violet w-[60%]`}
+                className={`bg-hungerbox-light-violet w-[60%] text-center`}
                 onChange={handleChange}
-                onKeyDown={handleKeyDown}
               />
             </div>
-            <div
-              className={`flex justify-center items-center mt-4`}
-            >
+            <div className={`flex justify-center items-center mt-6`}>
               {input && 
                 <Canvas 
                   text={input as string}
